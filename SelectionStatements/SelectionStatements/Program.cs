9dfc3de337a1017f4e6f527cba6adbd4db974752
Michael Scharffbillig <mscharffbillig@gmail.com>
2022-06-20 20:48:26 -0500

    initilized and tested random integer generator

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNum = r.Next(1, 1000);
+
+            Console.WriteLine(favNum);
+            Console.ReadLine();
+        }
+    }
+}
