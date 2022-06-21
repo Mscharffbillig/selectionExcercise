e88b0076b6c42e09a7064ae9a9263d2d74879cff
Michael Scharffbillig <mscharffbillig@gmail.com>
2022-06-20 21:14:28 -0500

    bug fixed- moved readline into switch statement. Works

--- Switch Statements/Switch Statements/Program.cs
+++ Switch Statements/Switch Statements/Program.cs
@@ -18,39 +18,41 @@ namespace Switch_Statements
                 case "math":
                     Console.WriteLine(favSubject);
                     Console.WriteLine("ah, algebra and geometry. Nice!");
+                    Console.ReadLine();
                     break;
 
                 case "science":
                     Console.WriteLine(favSubject);
-
                     Console.WriteLine("Chemistry and Physics. My favorite!");
+                    Console.ReadLine();
                     break;
 
                 case "history":
                     Console.WriteLine(favSubject);
-
                     Console.WriteLine("Kings and Conquerers. Good choice.");
+                    Console.ReadLine();
 
                     break;
 
                 case "english":
                     Console.WriteLine(favSubject);
-
                     Console.WriteLine("I too like the written word. Fascinating!");
+                    Console.ReadLine();
                     break;
 
                 case "gym":
                     Console.WriteLine(favSubject);
                     Console.WriteLine("One of those active people huh? Weird seeing you in coding classes.");
+                    Console.ReadLine();
                     break;
 
                 default:
                     Console.WriteLine(favSubject);
                     Console.WriteLine("Never thought about that...");
+                    Console.ReadLine();
                     break;
 
-                Console.ReadLine();
-
+                
             }
 
         }
